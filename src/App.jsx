1906c10e554660b9dc3fce1a57ca2c1708865e60
Olga <olenka15041991@gmail.com>
2022-08-22 20:53:13 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from './layouts/Layout'
+
+const App = () => {
+  return (
+    <Layout>
+      <div className='text-blue-900 font-bold text-2xl bg-slate-300'>App</div>
+    </Layout>
+  );
+};
+
+export default App
